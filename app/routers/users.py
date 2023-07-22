1e69b367941c9716049c3b0cd9d9b68701993e4b
joaovictordesouzaduarte <victorduarte.ufrj@gmail.com.br>
2023-07-22 20:54:31 -0300

    removing pycache

--- app/routers/users.py
+++ app/routers/users.py
@@ -3,7 +3,10 @@ from fastapi import APIRouter
 router = APIRouter()
 
 
-@router.get("/users")
+@router.get("/user")
 async def get_users():
     all_users = [{"name": "João Victor"}, {"name": "Márcia"}]
     return all_users
+
+
+# async def
